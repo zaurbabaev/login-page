856c1b24e644d9c8085801c5db57b537d343237a
Zaur <zaurik87@gmail.com>
2026-02-09 22:05:19 +0400

    update

--- Json-server-temp-deploy/api/server.js
+++ Json-server-temp-deploy/api/server.js
@@ -1,19 +1,26 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+import jsonServer from "json-server";
+import path from "path";
+import { fileURLToPath } from "url";
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// Export the Server API
-module.exports = server
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "../db.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+
+// Rewriter
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+    "/blog/:resource/:id/show": "/:resource/:id",
+  }),
+);
+
+server.use(router);
+
+// ✅ Serverless export
+export default server;
--- Json-server-temp-deploy/src/api/index.js
+++ Json-server-temp-deploy/src/api/index.js
@@ -1,7 +1,17 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_API_BASE || "http://localhost:3001",
+// });
+
+// export default api;
+
+
 import axios from "axios";
 
+// Vercel serverless backend URL
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:3001",
+  baseURL: "/api", // artıq Vercel deploy-da /api route serverless function-ə yönləndirilir
 });
 
 export default api;
