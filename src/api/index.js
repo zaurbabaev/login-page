e96b3d6bf2dd5ee861098cec406a815d9c3f8a2a
Zaur <zaurik87@gmail.com>
2026-02-09 21:02:59 +0400

    add env file

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:3001",
 });
 
 export default api;
